ac0eebd56e2c6d96ec3f575c3b9242f0b4896038
BLIZERTO <74712916+BLIZERTO@users.noreply.github.com>
2022-11-22 21:27:40 +0100

    Ajout ChatController create and getAll and delete

--- backend/src/src/Controller/ChatController.php
+++ backend/src/src/Controller/ChatController.php
@@ -4,22 +4,26 @@ use App\Entity\Chat;
 use App\Form\RoomType;
 use App\Repository\ChatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class ChatController extends AbstractController
 {
 
     public function __construct(ChatRepository $chatRepository) {
         $this->chatRepository = $chatRepository;
+
     }
 
     #[Route('/', name: 'app_all')]
-    public function index(): Response {
-       return $this->render('rooms/index.json', [
-           'allRooms' => $this->chatRepository->findAll(),
-       ]);
+    public function index(): JsonResponse {
+       return new JsonResponse(['allRooms' => $this->chatRepository->findAll()]);
     }
 
     #[Route('/creation', name: 'app_creation', methods: ["GET", "POST"])]
@@ -30,7 +34,20 @@ class ChatController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $room->setCreateur($this->getUser()->getId());
+            $this->chatRepository->save($room, true);
         }
+
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($room, 'json');
+        return new JsonResponse($jsonContent);
+    }
+
+    #[Route('/delete/{chat_id}', name: 'app_delete')]
+    public function deleteRoom(Request $request, int $chat_id) : JsonResponse {
+
     }
 
 }
