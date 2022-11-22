10f47eb77f8df181f8bf273eef05f37598ddf83e
Jérémy <jeremyschneider21@gmail.com>
2022-11-22 21:08:00 +0100

    add authenticator

--- backend/src/config/bundles.php
+++ backend/src/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
