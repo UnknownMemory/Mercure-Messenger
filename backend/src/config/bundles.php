92869652debaed4ca5d62aef33c2d90469bc7b94
Jérémy <jeremyschneider21@gmail.com>
2022-11-22 19:48:36 +0100

    add security bundle

--- backend/src/config/bundles.php
+++ backend/src/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
