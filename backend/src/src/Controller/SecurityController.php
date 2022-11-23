e554bb9b239f057849fe487ef530114a64f35bcb
Thomas-Torrente <thomas.torrente@gmail.com>
2022-11-23 09:40:34 +0100

    register controller

--- backend/src/src/Controller/SecurityController.php
+++ backend/src/src/Controller/SecurityController.php
@@ -3,28 +3,25 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register($request, UserPasswordHasherInterface $userPasswordHasher): Response
-    {   
-        $user = new User();
+    #[Route('/api/inscription', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
+        $entityManager->persist($user);
+        $entityManager->flush();
 
-        $user->setName($request->get('name'));
-        $user->setPassword(
-            $userPasswordHasher->hashPassword(
-                $user,
-                $request->get('plainPassword')->getData()
-            )
-        );
-
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+        return new JsonResponse('Utilisateur enregistré avec succès', Response::HTTP_CREATED, [], true);
     }
 }
