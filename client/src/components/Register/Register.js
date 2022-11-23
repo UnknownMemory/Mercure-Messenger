973a7889f8b901169c6b406d7045fe44e598d915
MadShiro <rayanatrouni@gmail.com>
2022-11-23 16:45:56 +0100

    register form complete
    formulaire

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 import useFetch from "../../hooks/useFetch";
+import "./register.css"
 
 const Register = () => {
     const [username, setUsername] = useState('')
@@ -16,12 +19,22 @@ const Register = () => {
     }
 
     return (
-        <form method="post" onSubmit={onSubmit}>
-            <input type="text" name="username" onChange={(e) => setUsername(e.currentTarget.value)} />
-            <input type="password" name="password" onChange={(e) => setPassword(e.currentTarget.value)}/>
-            <button type="submit">S'inscrire</button>
-        </form>
+        <Form onSubmit={onSubmit} id="formulaire">
+            <Form.Group classname="mt-3">
+                <Form.Label>Username</Form.Label>   
+                <Form.Control type="text" name="username" onChange={(e) => setUsername(e.currentTarget.value)} />
+            </Form.Group>
+            <Form.Group classname="mt-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" onChange={(e) => setPassword(e.currentTarget.value)}/>
+            </Form.Group>
+            <div className="mt-3 mb-3">
+                <Button variant="primary" type="submit">S'inscrire</Button>
+            </div>
+        </Form>
     );
 }
+
+
 
 export default Register;
