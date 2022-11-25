faa7889a5368f07e060c07f7a2cfefd557cb14e3
Thomas-Torrente <thomas.torrente@gmail.com>
2022-11-25 15:11:15 +0100

    remove isGranted and rename my Room

--- backend/src/src/Controller/ChatController.php
+++ backend/src/src/Controller/ChatController.php
@@ -12,12 +12,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
-use  Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/api/chat')]
 class ChatController extends AbstractController
 {
+
     public function __construct(ChatRepository $chatRepository, Security $security, UserRepository $userRepository)
     {
         $this->chatRepository = $chatRepository;
@@ -25,18 +25,25 @@ class ChatController extends AbstractController
         $this->userRepository = $userRepository;
     }
 
-    #[Route('/mesChats', name: 'my_room')]
-    #[IsGranted('ROLE_USER', message: 'Vous devez être connecter pour accéder aux tchats rooms')]
-    public function index(SerializerInterface $serializerInterface): JsonResponse {
-        $chatList = $this->chatRepository->findByUserChats($this->getUser());
 
-        $jsonChatList  = $serializerInterface->serialize($chatList, 'json', ['groups' => 'getChat']);
-        return new JsonResponse($jsonChatList, Response::HTTP_OK, [],true);
 
+    #[Route('/', name: 'my_room', methods: 'GET')]
+    public function index(SerializerInterface $serializerInterface): JsonResponse
+    {
+        if ($this->getUser()) {
+            $chatList = $this->chatRepository->findAllByUser($this->getUser());
+
+            $jsonChatList  = $serializerInterface->serialize($chatList, 'json', ['groups' => 'getChat']);
+
+            dd($jsonChatList);
+            return new JsonResponse($jsonChatList, Response::HTTP_OK, [], true);
+        } else {
+            return new JsonResponse('Vous devez être connecté pour accéder à cette page', Response::HTTP_UNAUTHORIZED, [], true);
+        }
     }
 
+
     #[Route('/creation', name: 'create_room', methods: ["GET", "POST"])]
-    #[IsGranted('ROLE_USER', message: 'Vous devez être connecter pour créer une chat room')]
     public function creationRooms(): Response
     {
         if ($this->getUser()) {
@@ -53,8 +60,8 @@ class ChatController extends AbstractController
         return new JsonResponse('Vous devez être connecté pour créer une chatRoom', Response::HTTP_UNAUTHORIZED, [], true);
     }
 
+
     #[Route('/{id}', name: 'one_room', methods: ['GET'])]
-    #[IsGranted('ROLE_USER', message: 'Vous devez être connecter pour accéder au tchat room')]
     public function oneRoom(Chat $chat): JsonResponse
     {
         if ($this->getUser() === $chat->getCreateur() || $this->getUser() === $chat->getParticipant()) {
@@ -65,10 +72,9 @@ class ChatController extends AbstractController
     }
 
     #[Route('/{id}/join', name: 'join_room', methods: ['GET'])]
-    #[IsGranted('ROLE_USER', message: 'Vous devez être connecter pour rejoindre un tchat room')]
     public function joinRoom(Chat $chat): JsonResponse
     {
-        if ($this->getUser()->getId() === $chat->getCreateur()->getId()) {
+        if ($this->getUser() === $chat->getCreateur()) {
             return new JsonResponse('Vous ne pouvez pas rejoindre votre propre chatRoom', Response::HTTP_UNAUTHORIZED, [], true);
         } else {
             $chat->setParticipant($this->getUser());
@@ -78,10 +84,9 @@ class ChatController extends AbstractController
     }
 
     #[Route('/{id}/leave', name: 'leave_room', methods: ['GET'])]
-    #[IsGranted('ROLE_USER', message: 'Vous devez être connecter pour quitter un tchat room')]
     public function leaveRoom(Chat $chat): JsonResponse
     {
-        if ($this->getUser()->getId() === $chat->getCreateur()->getId()) {
+        if ($this->getUser() === $chat->getCreateur()) {
             return new JsonResponse('Vous ne pouvez pas quitter votre propre chatRoom', Response::HTTP_UNAUTHORIZED, [], true);
         } else {
             $chat->setParticipant(null);
