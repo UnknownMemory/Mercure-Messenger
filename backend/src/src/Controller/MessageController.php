f5535ecf46168568c49826c115390561384049dc
Thomas-Torrente <thomas.torrente@gmail.com>
2022-12-01 13:04:35 +0100

    modify MessageController

--- backend/src/src/Controller/MessageController.php
+++ backend/src/src/Controller/MessageController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message')]
     public function index(): Response
     {
-        return $this->render('message/index.html.twig', [
-            'controller_name' => 'MessageController',
-        ]);
+        return new JsonResponse('rien pour le moment', Response::HTTP_NO_CONTENT, [], true);
     }
 }
