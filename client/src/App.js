45b70a5ac01e68aaee640e5bcaaba9df7f87a47d
Jérémy <jeremyschneider21@gmail.com>
2022-12-11 23:17:44 +0100

    update loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,40 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  redirect,
 } from "react-router-dom";
-import Cookies from "js-cookie";
 
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
-import MesTchats from "./components/MesTchats/MesTchats";
 
 import "./App.css";
 import Home from "./components/Home/Home";
 import ErrorPage from "./components/ErrorPage/ErrorPage"
+import { isNotLoggedLoader } from "./loaders/loaders";
 
 function App() {
-
-  const isNotLoggedLoader = async () => {
-    // if (!Cookies.get("auth")) {
-    //   return redirect("/login");
-    // };
-  };
-
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "*",
+      loader: isNotLoggedLoader,
+      element: <ErrorPage />, 
+    },
+    {
+      path: "/wetchat/:id",
+      element: <Home />,
       loader: isNotLoggedLoader,
       errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/wetchat/:id",
-          element: <Home />,
-        },
-        {
-          path: "/register",
-          element: <Register />,
-        },
-        {
-          path: "/login",
-          element: <Login />,
-        },
-        {
-          path: "/mes-tchats",
-          element: <MesTchats />,
-        },
-      ],
+    },
+    {
+      path: "/register",
+      loader: isNotLoggedLoader,
+      element: <Register />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      loader: isNotLoggedLoader,
+      element: <Login />,
+      errorElement: <ErrorPage />,
     },
   ]);
 
--- client/src/loaders/isLoggedLoader.js
+++
@@ -1,5 +0,0 @@
-import { redirect } from "react-router-dom";
-
-export async function isLoggedLoader() {
-    return redirect('/login');
-}
